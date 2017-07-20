2fad36ffbb06257f7ff3297c2061e972ce7578d9
snehalatha108 <sneha.thanguturu@gmail.com>
2017-07-20 10:11:17 +0530

    Initial commit

---
+++ app/src/main/java/com/enchcrop/andriodchat/UserDetails.java
@@ -0,0 +1,11 @@
+package com.enchcrop.andriodchat;
+
+/**
+ * Created by enchanter19 on 19/7/17.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
